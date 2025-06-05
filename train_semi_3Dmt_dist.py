2137ac640ade4b54fc022b249002a3c167931afb
r0pz <liuhongyu162@gmail.com>
2025-06-05 21:09:26 +0800

    Update train_semi_3Dmt_dist.py

--- train_semi_3Dmt_dist.py
+++ train_semi_3Dmt_dist.py
@@ -168,45 +168,23 @@ def main():
         model.train()
         is_best = False
         for i, ((img_x, mask_x),
-                (img_u_w, img_u_s1, img_u_s2, ignore_mask, cutmix_box1, cutmix_box2)) in enumerate(loader):
+                (img_u_w, img_u_s, ignore_mask)) in enumerate(loader):
             img_x, mask_x = img_x.cuda(), mask_x.cuda()
-            img_u_w, img_u_s1, img_u_s2 = img_u_w.cuda(), img_u_s1.cuda(), img_u_s2.cuda()
-            ignore_mask, cutmix_box1, cutmix_box2 = ignore_mask.cuda(), cutmix_box1.cuda(), cutmix_box2.cuda()
+            img_u_w, img_u_s = img_u_w.cuda(), img_u_s.cuda()
+            ignore_mask = ignore_mask.cuda()
             with torch.no_grad():           
                 pred_u_w = model_ema(img_u_w).detach()
                 conf_u_w = pred_u_w.softmax(dim=1).max(dim=1)[0]
                 mask_u_w = pred_u_w.argmax(dim=1)
             
-            img_u_s1[cutmix_box1.unsqueeze(1).expand(img_u_s1.shape) == 1] = img_u_s1.flip(0)[cutmix_box1.unsqueeze(1).expand(img_u_s1.shape) == 1]
-            img_u_s2[cutmix_box2.unsqueeze(1).expand(img_u_s2.shape) == 1] = img_u_s2.flip(0)[cutmix_box2.unsqueeze(1).expand(img_u_s2.shape) == 1]
-            
             pred_x = model(img_x)
-            pred_u_s1, pred_u_s2 = model(torch.cat((img_u_s1, img_u_s2)), comp_drop=True).chunk(2)
-            
-            mask_u_w_cutmixed1, conf_u_w_cutmixed1, ignore_mask_cutmixed1 = mask_u_w.clone(), conf_u_w.clone(), ignore_mask.clone()
-            mask_u_w_cutmixed2, conf_u_w_cutmixed2, ignore_mask_cutmixed2 = mask_u_w.clone(), conf_u_w.clone(), ignore_mask.clone()
-            
-            # cutmix
-            mask_u_w_cutmixed1[cutmix_box1 == 1] = mask_u_w.flip(0)[cutmix_box1 == 1]
-            conf_u_w_cutmixed1[cutmix_box1 == 1] = conf_u_w.flip(0)[cutmix_box1 == 1]
-            ignore_mask_cutmixed1[cutmix_box1 == 1] = ignore_mask.flip(0)[cutmix_box1 == 1]
-            
-            mask_u_w_cutmixed2[cutmix_box2 == 1] = mask_u_w.flip(0)[cutmix_box2 == 1]
-            conf_u_w_cutmixed2[cutmix_box2 == 1] = conf_u_w.flip(0)[cutmix_box2 == 1]
-            ignore_mask_cutmixed2[cutmix_box2 == 1] = ignore_mask.flip(0)[cutmix_box2 == 1]
-            
+            pred_u_s = model(img_u_s1)
+                 
             loss_x = criterion_l(pred_x, mask_x)
-
-            loss_u_s1 = criterion_u(pred_u_s1, mask_u_w_cutmixed1)
-            loss_u_s1 = loss_u_s1 * ((conf_u_w_cutmixed1 >= cfg['conf_thresh']) & (ignore_mask_cutmixed1 != 255))
-            loss_u_s1 = loss_u_s1.sum() / (ignore_mask_cutmixed1 != 255).sum().item()
-            
-            loss_u_s2 = criterion_u(pred_u_s2, mask_u_w_cutmixed2)
-            loss_u_s2 = loss_u_s2 * ((conf_u_w_cutmixed2 >= cfg['conf_thresh']) & (ignore_mask_cutmixed2 != 255))
-            loss_u_s2 = loss_u_s2.sum() / (ignore_mask_cutmixed2 != 255).sum().item()
-            
+            loss_u_s = criterion_u(pred_u_s, mask_u_w)
+            loss_u_s = loss_u_s * ((conf_u_w >= cfg['conf_thresh']) & (ignore_mask != 255))
+            loss_u_s = loss_u_s.sum() / (ignore_mask != 255).sum().item()
             consistency_weight = get_current_consistency_weight(iter_num // 200)
-            loss_u_s = (loss_u_s1 + loss_u_s2) / 2.0
             loss = (loss_x + loss_u_s) / 2.0
             
             iter_num = iter_num + 1
@@ -218,8 +196,6 @@ def main():
             total_loss.update(loss.item())
             total_loss_x.update(loss_x.item())
             total_loss_s.update(loss_u_s.item())
-            total_loss_s1.update(loss_u_s1.item())
-            total_loss_s2.update(loss_u_s2.item())
             mask_ratio = ((conf_u_w >= cfg['conf_thresh']) & (ignore_mask != 255)).sum().item() / (ignore_mask != 255).sum()
             total_mask_ratio.update(mask_ratio.item())
 
@@ -240,8 +216,6 @@ def main():
                 writer.add_scalar('train/lr', lr, iters)
                 writer.add_scalar('train/loss_x', loss_x.item(), iters)
                 writer.add_scalar('train/loss_s', loss_u_s.item(), iters)
-                writer.add_scalar('train/loss_s1', loss_u_s1.item(), iters)
-                writer.add_scalar('train/loss_s2', loss_u_s2.item(), iters)
                 writer.add_scalar('train/mask_ratio', mask_ratio, iters)
 
                 if (i % (len(trainloader_u) // 3) == 0):
